950f7e3e9f02990f9ca58771534a09c42a51615f
Shivam <shivamkpandeygc@gmail.com>
2025-06-08 19:36:35 +0000

    data

---
+++ js/datatypes.js
@@ -0,0 +1,17 @@
+//learn. about datatypes
+"use strict" ;
+
+// alert(3+3)   // not in node but in browser
+
+let name ="shivam "; //string
+let age = 23 ; //number //bigint
+//boolean
+//null. stand alone value
+//undefined
+//symbol =>uniqueness
+// objejct 
+//
+
+console.log(typeof 4);
+console.log(typeof null); //object type
+console.log(typeof undefined); //undefined type
---
+++ js/var.js
@@ -0,0 +1,9 @@
+const accountId = 12345;
+let accountEmail = "spst@gmail" ;
+var accountPassword = "Shivam1122@";  //prefered not to use var becasue of block and functional scope cause it has no scope
+accountCity = "Pithoragarh" ;  //should not use
+let accountState ;     // initially undefined
+
+// accountId = 123 ;     not allowed
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
